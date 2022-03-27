ef4db5ba4fea7d50ad6912e9173414df768b17f1
MotasemZiad <miziad2000@gmail.com>
2022-03-27 13:56:33 +0300

    new commit

--- lec1.php
+++ lec1.php
@@ -1,33 +1,43 @@
-<?php
+<?php 
+    $name = "Motasem Abu Nema";
+    $age = 19;
+    $commission = 0.1;
+    $is_student = false;
+    $courses = ["Web Development using PHP", "Android Development using Kotlin", "Game Development using Unity(C#)", "Entrepreneurship", "Design Patterns", "Graduation Project",];
 
-function add($n1, $n2){
-    return $n1 + $n2;
-}
+    $add = 9 + 2;
+    $sub = 9 - 2;
+    $mul = 9 * 2;
+    $div = 9 / 2;
+    $mod = 9 % 2;
+    $power = 9 ** 2;
+    /* Power of the operators
+    0. ()
+    1. **
+    2. *
+    3. /
+    4. +
+    5. -
+    6. %
+    */
+    $arr = [5, 6, 12, 93, 23, 53, 26, 72,];
+    $n = 12.6;
+    echo sin($n)."<br>";
+    echo cos($n)."<br>";
+    echo abs($n)."<br>";
+    echo log($n)."<br>";
+    echo ceil($n)."<br>";
+    echo floor($n)."<br>";
+    echo max($arr)."<br>";
+    echo sqrt(81)."<br>";
+    echo M_PI."<br>";
+    echo M_E."<br>";
+    $num = 4;
+    $total = $num * 14 - 1 + 72 /M_E * 5 + sqrt(14.5) - 1.1; 
+    echo "Total is => $total<br>";
 
-function sub($n1, $n2){
-    return $n1 - $n2;
-}
-
-function mul($n1, $n2){
-    return $n1 * $n2;
-}
-
-function div($n1, $n2){
-    if($n2 == 0){
-        throw new Exception("Divide by ZERO");
-    }else {
-        return $n1 / $n2;
+    echo "9 & 2=> $add, $sub, $mul, $div, $mod, $power<br>";
+    echo "Name: $name<br>Age: $age<br>Commission: $commission<br>Is Student: $is_student<br>Courses:<br>";
+    for ($i=0; $i < count($courses); $i++) { 
+        echo "$courses[$i]<br>";
     }
-}
-echo add(8, 15);
-echo "<br>";
-echo sub(8, 15);
-echo "<br>";
-echo mul(8, 15);
-echo "<br>";
-
-try{
-    echo sub(9, 0);
-}catch (Exception $e){
-    echo $e;
-}
