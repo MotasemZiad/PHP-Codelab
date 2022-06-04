c0a40440eda2a516bfd2ce5a3795861c0879c071
MotasemZiad <miziad2000@gmail.com>
2022-06-04 03:52:31 +0300

    update form

---
+++ forms/first.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+
+    function generate_file_name ($length) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $characters_length = strlen($characters);
+        $random_string = '';
+        for ($i = 0; $i < $length; $i++) {
+            $random_string .= $characters[rand(0, $characters_length - 1)];
+        }
+        return $random_string;
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Example</title>
+</head>
+<body>
+    <form action="" method="POST" enctype="multipart/form-data"></form>
+</body>
+</html>
